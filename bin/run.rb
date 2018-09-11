58074473c3049f6463646d33b73d9e79cfd195ed
github username <github email address>
2018-09-11 14:24:56 -0400

    added command line logic

--- bin/run.rb
+++ bin/run.rb
@@ -1,2 +1,6 @@
-require_relative 'config/environment'
+require_relative '../config/environment.rb'
 
+
+
+new_cli = CommandLineInterface.new
+new_cli.run 
--- lib/app/models/commandlineinterface.rb
+++ lib/app/models/commandlineinterface.rb
@@ -1,2 +1,36 @@
 class CommandLineInterface
+
+  def run
+    greet
+    input = gets_user_input.to_i
+    if input == 1
+      create_an_account
+    end
+  end
+
+
+  def greet
+    puts "Welcome to Game Master 3000!"
+    puts "Lets begin!"
+  end
+
+  def gets_user_input
+    puts "Choose an option:\n 1.)Create an account\n 2.)See whose playing\n 3.)Start_playing"
+    input = gets.chomp
+    # if input > 3
+    #   puts "invalid input"
+    # end
+  end
+
+
+
+  def create_an_account
+    puts "What is your name?"
+    name = gets.chomp
+    puts "What is your location"
+    location = gets.chomp
+    puts "What is your age"
+    age = gets.chomp
+  end
+
 end
