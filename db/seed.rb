241e331dd67bd62726aa3c05cfef04f4b7dedae8
jb5595 <jacobberman1995@gmail.com>
2018-09-11 11:35:06 -0400

    added game attributes to seed

--- db/seed.rb
+++ db/seed.rb
@@ -11,17 +11,7 @@ player2= Player.find_or_create_by(name: "player2")
 player3= Player.find_or_create_by(name: "player3")
 player4= Player.find_or_create_by(name: "player4")
 
-game1 = Game.find_or_create_by(name: "game1")
-game2 = Game.find_or_create_by(name: "game2")
-game3 = Game.find_or_create_by(name: "game3")
-game4 =Game.find_or_create_by(name: "game4")
-
-player1.location = "EU"
-player2.location = "AM"
-player3.location = "AM"
-player4.location = "EU"
-
-player1.age = 22
-player2.age = 14
-player3.age = 17
-player4.age = 19
+game1 = Game.find_or_create_by(name: "game1", genre: "action")
+game2 = Game.find_or_create_by(name: "game2", genre: "adventure")
+game3 = Game.find_or_create_by(name: "game3", genre: "hack and slash")
+game4 =Game.find_or_create_by(name: "game4", genre: "adventure")
