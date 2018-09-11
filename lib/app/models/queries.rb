2f72bb30eb87b069f5d6b34b57ff287cb1546a7b
jb5595 <jacobberman1995@gmail.com>
2018-09-11 14:01:55 -0400

    added querie class and find_average_age_of_game method

--- lib/app/models/queries.rb
+++ lib/app/models/queries.rb
@@ -1,13 +1,17 @@
 class Querie
 
+# Find average age of players of passed in game
   def self.find_average_age_of_game(game)
-    ages = Player.all.collect do |player|
+    #
+    ages = Player.all.collect do |player| ## Retrieves array of all player objects
+      # checks to see if players game array contains the passed in game
       if player.games.include?(game1)
+        # collects the players age
         player.age
       end
-    end.compact
+    end.compact #Removes nil space
 
-    average = ages.sum.to_f/ages.length.to_f
+    average = ages.sum.to_f/ages.length.to_f #Average = total/number of instances
   end
 
 end
